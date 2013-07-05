c1de75723336e17701f6864cf3341a5d17f01a8e
ansond <>
2013-07-05 19:13:36 +0000

    socket.io compatible library on top of the websockets API

---
+++ SocketIO.cpp
@@ -0,0 +1,192 @@
+#include "SocketIO.h"
+
+// constructor
+SocketIO::SocketIO(char * myurl) {
+    // prepare the sessionkey URL
+    this->prepareSessionKeyURL(myurl, DEFAULT_VERSION);
+}
+
+// constructor
+SocketIO::SocketIO(char * myurl, int myversion) {
+    // prepare the sessionkey URL
+    this->prepareSessionKeyURL(myurl, myversion);    
+}
+
+// connect to the SocketIO server
+bool SocketIO::connect() {
+    bool connected = this->is_connected();
+    
+    // make sure we are not already connected
+    if (connected == false) {
+        // first we must acquire the session key
+        bool connected = this->acquireSessionKey();
+        
+        // next we must create a new URL to connect to with the session key
+        if (connected == true) {
+            // create our session URL
+            this->prepareSessionURL();
+        
+            // allocate our websocket endpoint
+            this->ws = new Websocket(this->url_session);
+            
+            // connect!
+            connected = this->attemptWebSocketConnect();
+        }
+    }
+    
+    // return our connection status
+    return connected;
+}
+
+// gracefully disconnect from the SocketIO server
+bool SocketIO::close() {
+    bool closed = false;
+  
+    // if we are connected lets disconnect
+    if (this->ws != NULL) {
+        this->ws->close();
+        closed = this->ws->is_connected();
+    }
+    
+    // now clean up memory 
+    if (this->ws != NULL) delete this->ws;
+    if (this->url_session != NULL) delete this->url_session;
+    if (this->url_session_key != NULL) delete this->url_session_key;
+    if (this->session_key != NULL) delete this->session_key;
+    if (this->ws_channel != NULL) delete this->ws_channel;
+    
+    // return our status
+    return closed;
+}
+
+// emit a message (broadcast) to the SocketIO server
+int SocketIO::emit(char *name, char * args) {
+    int bytesSent = 0;
+    
+    // only emit if we are connected
+    if (this->is_connected() == true) {
+        // prepare the JSON message for SocketIO
+        char buffer[256];
+        char *json = this->prepareSocketIOJSONMessage(name,args,buffer);
+        
+        // send the message
+        bytesSent = this->ws->send(json);
+    }
+    
+    return bytesSent;
+}
+
+// read a message from the SocketIO server
+bool SocketIO::read(char * message) {
+    bool didRead = false;
+    
+    // only read if we are connected
+    if (this->is_connected() == true && message != NULL) {        
+        // attempt a read
+        didRead = this->ws->read(message);        
+    }
+    
+    // return our status
+    return didRead;
+}
+
+// Are we connected?
+bool SocketIO::is_connected() {
+    bool isConnected = false;
+    
+    // if we have an endpoint - get its connection status
+    if (this->ws != NULL) isConnected = this->ws->is_connected();
+    
+    return isConnected;
+}
+
+// Private Methods
+
+// prepare a SocketIO compatible JSON packet
+char *SocketIO::prepareSocketIOJSONMessage(char *name, char *args, char *buffer) {
+    sprintf(buffer,"5:::{\"name\": \"%s\", \"args\": \"%s\"}",name, args);
+    return buffer;
+}
+
+// prepare the session URL
+void SocketIO::prepareSessionURL() {
+    // allocate the buffer
+    this->url_session = new char[256];
+    
+    // fill the buffer
+    sprintf(this->url_session,"ws://%s/%s/%s",this->url,this->ws_channel,this->session_key);
+}
+
+// attempt a connection via websockets
+bool SocketIO::attemptWebSocketConnect() {
+    // attempt connect
+    int status = 0;
+    for(int i=0;i<10;++i) {
+        status = this->ws->connect();
+        if (status != 0) i = 10;
+    }
+    
+    // set the socket.io connect command
+    if (this->ws->is_connected()) this->ws->send("1::");
+    
+    // return connection status
+    return this->is_connected();
+}    
+
+// prepare the session key URL
+void SocketIO::prepareSessionKeyURL(char *myurl, int myversion) {
+    // save the base URL
+    this->url = myurl;
+    
+    // set our version
+    this->version = myversion;
+    
+    // build out the session key URL
+    this->url_session_key = new char[256];
+    time_t seconds = time(NULL);
+    sprintf(this->url_session_key,"http://%s/socket.io/%d/?t=%d",this->url,this->version,seconds);
+    
+    // setup the session key and channel buffers
+    this->session_key = new char[128];
+    this->ws_channel = new char[128];
+}
+
+// parse the socket.io session key
+void SocketIO::parseSessionKey(char *response, char *sessionkey, char *channel) {
+     int val1 = 0;
+     int val2 = 0;
+     
+     // convert ":" to " "
+     for(int i=0;i<strlen(response);++i) if (response[i] == ':') response[i] = ' ';
+     
+     // format:  Session_ID YY ZZ CHANNEL
+     char t_sessionkey[128];
+     sscanf(response,"%s %d %d %s",t_sessionkey,&val1,&val2,channel);
+     
+     // create
+     sprintf(sessionkey,"%s/%s",channel,t_sessionkey);            
+}
+
+// acquire the session key for our session
+bool SocketIO::acquireSessionKey() {
+    bool haveKey = false;
+    HTTPClient http;
+    char response[513];
+    
+    // make sure we have buffers
+    if (this->session_key != NULL && this->ws_channel != NULL) {
+        // request our session key
+        int nread = http.get(this->url_session_key,response,512);
+        
+        // parse the session key
+        if (!nread)         
+             // parse into the session key
+             this->parseSessionKey(response,this->session_key,this->ws_channel);
+        
+        // update our status
+        if (strlen(this->session_key) > 0) haveKey = true;
+    }
+        
+    // return status
+    return haveKey;
+}
---
+++ SocketIO.h
@@ -0,0 +1,159 @@
+/**
+* @author Doug Anson
+*
+* @section LICENSE
+*
+* Copyright (c) 2013 mbed
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+* @section DESCRIPTION
+*    Simple SocketIO client library
+*
+*/
+
+#ifndef SOCKETIO_H
+#define SOCKETIO_H
+
+#include "mbed.h"
+
+// Default SocketIO version
+#define DEFAULT_VERSION  1
+
+// HTTP support for session key retrieval
+#include "HTTPClient.h"
+
+// WebSocket layer support
+#include "Websocket.h"
+
+/** SocketIO client Class.
+ *
+ * Example (ethernet network):
+ * @code
+ * #include "mbed.h"
+ * #include "WiflyInterface.h"
+ * #include "SocketIO.h"
+ * 
+ * // our wifi interface
+ * WiflyInterface wifly(p9, p10, p30, p29, "mysid", "mypw", WPA); 
+ * 
+ * int main() {
+ *    wifly.init(); //Use DHCP
+ *    wifly.connect();
+ *    printf("IP Address is %s\n\r", wifly.getIPAddress());
+ *   
+ *    SocketIO socketio("myapp.herokuapp.com");
+ *    socketio.connect();
+ *   
+ *    char recv[256];
+ *    while (1) {
+ *       int res = socketio.emit("mesage_name","[\"SocketIO Hello World!\"]");
+ *        if (socketio.read(recv)) {
+ *            printf("rcv: %s\r\n", recv);
+ *        }
+ *        wait(0.1);
+ *    }
+ * }
+ * @endcode
+ */
+ 
+class SocketIO
+{
+    public:
+        /**
+        * Constructor
+        *
+        * @param url The SocketIO url in the form "www.example.com:[port]" (by default: port = 80) - i.e. just the endpoint name
+        */
+        SocketIO(char * url);
+        
+        /**
+        * Constructor
+        *
+        * @param url The SocketIO url in the form "www.example.com:[port]" (by default: port = 80) - i.e. just the endpoint name
+        * @param version The SocketIO version for the session URL (by default version = 1)
+        */
+        SocketIO(char * url, int version);
+
+        /**
+        * Connect to the SocketIO url
+        *
+        *@return true if the connection is established, false otherwise
+        */
+        bool connect();
+
+        /**
+        * Emit (Broadcast) a socket.io message to the SocketIO server
+        *
+        * Socket.IO message Format (JSON): { "name": <name>, "args":<args> }
+        *       name: the "name" of the socket.io message
+        *       args: the argument(s) (must always be a JSON array) of the message. Example: "[ \"foo\", {\"bar\": \"none\"}]" 
+        *
+        * @param name "name" of the socket.io message to broadcast
+        * @param args argument string to be sent ( must be in a JSON array format. Example: "[ \"foo\", {\"bar\": \"none\"}]" )
+        *
+        * @returns the number of bytes sent
+        */
+        int emit(char * name, char * args);
+
+        /**
+        * Read a SocketIO message
+        *
+        * @param message pointer to the string to be read (null if drop frame)
+        *
+        * @return true if a SocketIO frame has been read
+        */
+        bool read(char * message);
+
+        /**
+        * To see if there is a SocketIO connection active
+        *
+        * @return true if there is a connection active
+        */
+        bool is_connected();
+
+        /**
+        * Close the SocketIO connection
+        *
+        * @return true if the connection has been closed, false otherwise
+        */
+        bool close();
+
+    private:
+        // Variables
+        int         version;            // default socket.io version
+        char        *url;               // base URL endpoint to connect to. Example: "myapp.herokuapp.com"
+        char        *url_session_key;   // generated session key URL - used to extract the session key 
+        char        *url_session;       // session URL - specific socket.io session to bind to
+        Websocket   *ws;                // websocket endpoint
+        char        *session_key;       // our session key
+        char        *ws_channel;        // our websocket channel for the session
+        
+        // Methods
+        char *prepareSocketIOJSONMessage(char *name, char *args, char *buffer);
+        void prepareSessionURL();
+        bool attemptWebSocketConnect();
+        void parseSessionKey(char *response, char *sessionkey, char *ws_channel);
+        bool acquireSessionKey();
+        void prepareSessionKeyURL(char *myurl, int myversion);
+        
+        
+};
+
+#endif
